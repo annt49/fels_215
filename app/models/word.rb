28d0e3ce21179c954f97b58596cdef86e62a92e5
Nguyen Thi An <nguyen.thi.an@framgia.com>
2016-12-07 11:28:21 +0700

    filter words

--- app/models/word.rb
+++ app/models/word.rb
@@ -19,6 +19,18 @@ class Word < ApplicationRecord
     where("words.content LIKE ? AND is_correct = ?", "%#{value}%", true).
     select("words.content as word_content, answers.content as answer_content")}
 
+  scope :filter_by_category, -> (category_id){where "category_id = ?",
+    category_id if category_id.present?}
+  scope :all_words, -> user_id{}
+  scope :learned, -> (user_id){where "words.id in (SELECT r. word_id
+    FROM lessons l join results r
+    on l.id = r.lesson_id join answers a on r.word_id = a.word_id
+    where l.user_id = ? AND a.is_correct = ?)", user_id, true}
+  scope :not_learned, -> (user_id){where "words.id not in (SELECT r. word_id
+    FROM lessons l join results r
+    on l.id = r.lesson_id join answers a on r.word_id = a.word_id
+    where l.user_id = ? AND a.is_correct = ?)", user_id, true}
+
   class << self
     def search value
       if value.present?
